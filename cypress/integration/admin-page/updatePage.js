4beca4d44fc1cdb9f94988afa195aba64be4b3b2
Jhonnatan Eduardo Zamudio Palacios <j.zamudiop@uniandes.edu.co>
2021-05-09 12:40:26 -0500

    Se implementa el login para el escenario 2

---
+++ cypress/integration/admin-page/updatePage.js
@@ -0,0 +1,22 @@
+/// <reference types="cypress" />
+var data;
+context('Actions', () => {
+    before(function () {
+        cy.clearCookies()
+        cy.fixture('login_credentials').then(function (data) {
+            this.data = data;
+        })
+    })
+
+    beforeEach(() => {
+        Cypress.Cookies.preserveOnce('ghost-admin-api-session')
+    })
+
+    it('Fill username & password then log in ', function () {
+        cy.visit('http://localhost:2368/ghost/#/signin')
+        cy.get('.email').type(this.data.username).debug()
+        cy.get('.password').type(this.data.password)
+        cy.get('.login').click()
+        cy.wait(500)
+    })
+});
