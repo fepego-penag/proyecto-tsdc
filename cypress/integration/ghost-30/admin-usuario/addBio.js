f5b5407836c462bd7ab93315f97d03b8f80b7ac2
Jhonnatan Eduardo Zamudio Palacios <j.zamudiop@uniandes.edu.co>
2021-05-16 20:11:41 -0500

    Se agregan las imagenes del escenario addBio.js con la versión 3.3.0 Ghost

--- cypress/integration/ghost-30/admin-usuario/addBio.js
+++ cypress/integration/ghost-30/admin-usuario/addBio.js
@@ -18,6 +18,7 @@ context('Actions', () => {
         cy.get('.password').type(this.data.password)
         cy.get('.login').click()
         cy.wait(500)
+        cy.screenshot()
     })
 
     it('In the dashboard select the tab "Staff and enter the user Ghost', function () {
@@ -25,12 +26,14 @@ context('Actions', () => {
         cy.wait(500)
         cy.contains('Ghost').click()
         cy.wait(500)
+        cy.screenshot()
     })
 
     it('Clean up the bio and write', function () {
         cy.get('textarea[id="user-bio"]').clear()
         cy.get('textarea[id="user-bio"]').type('Somos estudiantes de la maestría en ingeniería de software, actualmente nos encontramos en proceso de aprendizaje para aplicar pruebas E2E con la api de automatización Cypress')
         cy.wait(500)
+        cy.screenshot()
     })
 
     it('We save the changes and return to the staff section', function () {
@@ -38,11 +41,13 @@ context('Actions', () => {
         cy.wait(500)
         cy.contains('Staff').parent().find('a').click()
         cy.wait(500)
+        cy.screenshot()
     })
 
     it('Re-enter the Ghost user to verify the changes in the biography', function () {
         cy.contains('Ghost').click()
         cy.wait(500)
         cy.get('textarea[id="user-bio"]').type(' ')
+        cy.screenshot()
     })
 });
