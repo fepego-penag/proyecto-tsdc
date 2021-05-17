98663cb5b7fd0b91ec7e89fc976355029643f946
Jhonnatan Eduardo Zamudio Palacios <j.zamudiop@uniandes.edu.co>
2021-05-16 20:42:14 -0500

    Se corrige script publisPage.js para que funcione con la nueva versión de Ghost 3.42.5

--- cypress/integration/ghost-34/admin-page/publishPage.js
+++ cypress/integration/ghost-34/admin-page/publishPage.js
@@ -18,11 +18,13 @@ context('Actions', () => {
         cy.get('.password').type(this.data.password)
         cy.get('.login').click()
         cy.wait(500)
+        cy.screenshot()
     })
 
     it('Select page administration', function () {
         cy.contains('Pages').parent().find('a').click()
         cy.wait(500)
+        cy.screenshot()
     })
 
     it('click on button "New Page", fill form page title and check if new page appears on list', function () {
@@ -30,19 +32,23 @@ context('Actions', () => {
         cy.get('textarea[placeholder="Page Title"]').type('Mi tercer página')
         cy.get('div[data-kg="editor"]').click()
         cy.visit('http://localhost:2368/ghost/#/pages')
+        cy.screenshot()
     })
 
     it('Search the page "Mi tercer página" and select', function () {
-        cy.get('a[title="Edit this post"]').contains('Mi tercer página').click()
+        cy.get('a[title="Edit this page"]').contains('Mi tercer página').click()
+        cy.screenshot()
     })
 
     it('click on menu "Publish" and click button publish', function () {
         cy.contains('Publish').parent().find('span').click()
         cy.get('button').contains('Publish').parent().find('span').click()
+        cy.screenshot()
     })
 
     it('Check if new page appears on list', function () {
         cy.contains('Pages').parent().find('a').click()
         cy.wait(500)
+        cy.screenshot()
     })
 });
