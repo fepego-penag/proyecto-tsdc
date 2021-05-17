fd1c20338528cd8ad9a6580e56780d902069ee7f
Jhonnatan Eduardo Zamudio Palacios <j.zamudiop@uniandes.edu.co>
2021-05-16 20:38:23 -0500

    Se ajusta el escenario deletePage.js para que funcione con la nueva version 3.42.5 de ghost y se capturan las fotos

--- cypress/integration/ghost-34/admin-page/deletePage.js
+++ cypress/integration/ghost-34/admin-page/deletePage.js
@@ -18,11 +18,13 @@ context('Actions', () => {
         cy.get('.password').type(this.data.password)
         cy.get('.login').click()
         cy.wait(500)
+        cy.screenshot()
     })
 
     it('Select page administration', function () {
         cy.contains('Pages').parent().find('a').click()
         cy.wait(500)
+        cy.screenshot()
     })
 
     it('click on button "New Page", fill form page title and check if new page appears on list', function () {
@@ -31,26 +33,31 @@ context('Actions', () => {
         cy.get('div[data-kg="editor"]').click()
         cy.visit('http://localhost:2368/ghost/#/pages')
         cy.wait(500)
+        cy.screenshot()
     })
 
     it('Search the page "Mi cuarta página", select page and click on menu "Settings"', function () {
-        cy.get('a[title="Edit this post"]').contains('Mi cuarta página').click()
+        cy.get('a[title="Edit this page"]').contains('Mi cuarta página').click()
         cy.get('button[title="Settings"]').click()
         cy.wait(500)
+        cy.screenshot()
     })
 
     it('Scroll down to the bottom and select "Delete Page"', function () {
         cy.get('button').contains('Delete').parent().find('span').click()
         cy.wait(500)
+        cy.screenshot()
     })
 
     it('Confirm the pop-up message by clicking on the "Delete" option', function () {
         cy.get('div[class="modal-footer"]').contains('Delete').click({force:true})
         cy.wait(500)
+        cy.screenshot()
     })
 
     it('Check if new page appears on list', function () {
         cy.visit('http://localhost:2368/ghost/#/pages')
         cy.wait(500)
+        cy.screenshot()
     })
 });
